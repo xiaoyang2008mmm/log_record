7626ebc3976acca3a818c33c06e9a1cc7e711cfd
root <root@localhost.(none)>
2015-08-07 23:34:00 +0800

    修改js获取第一个span的bug

--- static/js/zhangyue.js
+++ static/js/zhangyue.js
@@ -46,14 +46,21 @@ $(document).ready(function(){
 		}
 /////////////////////////////////////
 // 
-  $("#tab_span td span:first").click(function(){
+  $("#tab_span td span:first-child").click(function(){
 	alert($(this).text());
 	$.post("/change_log/",
                 {
                         id:$(this).text(),
 		},
 			function(data){
-     				alert(decodeURI(data));
+     				//alert(decodeURI(data));
+     				alert(data);
+
+
+			$.each(data,function(i, n){ 
+				console.info("Name: " + i + ", Value: " + n); 
+				});
+
                      });
 	
 	 });
